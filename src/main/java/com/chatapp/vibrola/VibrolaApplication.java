a921f57a64cc09b02724752c432935824d31d9f0
dhanur350 <dhanur297@gmail.com>
2025-11-19 21:54:26 +0530

    initial commit

---
+++ src/main/java/com/chatapp/vibrola/VibrolaApplication.java
@@ -0,0 +1,13 @@
+package com.chatapp.vibrola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibrolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibrolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chatapp/vibrola/VibrolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatapp.vibrola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibrolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
